9e81896003f1bd362f4e8063558c605fc9e633b6
Oded Sofer <oded6560@gmail.com>
2021-10-08 16:41:46 +0300

    added merging action inside the diraction functions

--- app.js
+++ app.js
@@ -10,17 +10,26 @@ const tiles = [
     [0, 0, 0, 0],
     [0, 0, 0, 0]
 ];
-const blc2 = document.createElement('div');
-blc2.classList.add('blc2', 'innerBlock');
-blc2.innerText = '2';
+let firstRandom = Math.floor(Math.random() * 16) + 1
+tiles[Math.floor(firstRandom / 4)][(firstRandom - 1) % 4] = 2;
+let secondRandom = firstRandom;
+while (secondRandom === firstRandom) {
+    secondRandom = Math.floor(Math.random() * 16) + 1
+}
+if (Math.floor(Math.random() * 6) + 1 === 1) {
+    tiles[Math.floor(secondRandom / 4)][(secondRandom - 1) % 4] = 4;
+}
+else {
+    tiles[Math.floor(secondRandom / 4)][(secondRandom - 1) % 4] = 2;
+
+}
+
+const Fblc = document.createElement('div');
+Fblc.classList.add('blc2', 'innerBlock');
+Fblc.innerText = '2';
 const block = document.querySelector('#p-1-1')
 block.appendChild(blocks[5]);
 document.addEventListener('keydown', (e) => { if (!e.repeat) { keyMap(e.key) } })
-// let gameOn = false;
-// while (gameOn) {
-
-
-// }
 function keyMap(key) {
     const tilesCopy = [[...tiles[0]], [...tiles[1]], [...tiles[2]], [...tiles[3]]];
     if (key === 'ArrowUp') {
@@ -30,20 +39,31 @@ function keyMap(key) {
         tiles = down(tilesCopy);
     }
     else if (key === 'ArrowLeft') {
+        tiles = left(tilesCopy);
 
     }
     else if (key === 'ArrowRight') {
+        tiles = right(tilesCopy);
 
     }
 }
 
 function down(tilesCopy) {
     for (let row = 0; row < 4; row++) {
+        let mergeFlag = false;
         const newRow = [];
         for (let i = 3; i >= 0; i--) {
             if (tilesCopy[i][row] !== 0) {
                 newRow.push(tilesCopy[i][row]);
             }
+        }
+        for (let i = 0; i < newRow.length - 1; i++) {
+            if (newRow[i] === newRow[i + 1]) {
+                newRow[i] = newRow[i] * 2;
+                newRow[i + 1] = 0;
+                mergeFlag = true;
+            }
+
         }
         for (let i = newRow.length; i < 4; i++) {
             newRow.push(0);
@@ -53,6 +73,9 @@ function down(tilesCopy) {
             tilesCopy[i][row] = newRow[3 - i];
 
         }
+        if (mergeFlag) {
+            tilesCopy = down(tilesCopy);
+        }
     }
     return tilesCopy;
 
@@ -61,11 +84,20 @@ function down(tilesCopy) {
 
 function right(tilesCopy) {
     for (let row = 0; row < 4; row++) {
+        let mergeFlag = false;
         const newRow = [];
         for (let i = 3; i >= 0; i--) {
             if (tilesCopy[row][i] !== 0) {
                 newRow.push(tilesCopy[row][i]);
             }
+        }
+        for (let i = 0; i < newRow.length - 1; i++) {
+            if (newRow[i] === newRow[i + 1]) {
+                newRow[i] = newRow[i] * 2;
+                newRow[i + 1] = 0;
+                mergeFlag = true;
+            }
+
         }
         for (let i = newRow.length; i < 4; i++) {
             newRow.push(0);
@@ -75,6 +107,9 @@ function right(tilesCopy) {
             tilesCopy[row][i] = newRow[3 - i];
 
         }
+        if (mergeFlag) {
+            tilesCopy = down(tilesCopy);
+        }
     }
     return tilesCopy;
 
@@ -83,11 +118,20 @@ function right(tilesCopy) {
 
 function up(tilesCopy) {
     for (let row = 0; row < 4; row++) {
+        let mergeFlag = false;
         const newRow = [];
         for (let i = 0; i < 4; i++) {
             if (tilesCopy[i][row] !== 0) {
                 newRow.push(tilesCopy[i][row]);
             }
+        }
+        for (let i = 0; i < newRow.length - 1; i++) {
+            if (newRow[i] === newRow[i + 1]) {
+                newRow[i] = newRow[i] * 2;
+                newRow[i + 1] = 0;
+                mergeFlag = true;
+            }
+
         }
         for (let i = newRow.length; i < 4; i++) {
             newRow.push(0);
@@ -97,6 +141,9 @@ function up(tilesCopy) {
             tilesCopy[i][row] = newRow[i];
 
         }
+        if (mergeFlag) {
+            tilesCopy = down(tilesCopy);
+        }
     }
     return tilesCopy;
 
@@ -105,11 +152,20 @@ function up(tilesCopy) {
 
 function left(tilesCopy) {
     for (let row = 0; row < 4; row++) {
+        let mergeFlag = false;
         const newRow = [];
         for (let i = 0; i < 4; i++) {
             if (tilesCopy[row][i] !== 0) {
                 newRow.push(tilesCopy[row][i]);
             }
+        }
+        for (let i = 0; i < newRow.length - 1; i++) {
+            if (newRow[i] === newRow[i + 1]) {
+                newRow[i] = newRow[i] * 2;
+                newRow[i + 1] = 0;
+                mergeFlag = true;
+            }
+
         }
         for (let i = newRow.length; i < 4; i++) {
             newRow.push(0);
@@ -118,6 +174,9 @@ function left(tilesCopy) {
 
             tilesCopy[row][i] = newRow[i];
 
+        }
+        if (mergeFlag) {
+            tilesCopy = down(tilesCopy);
         }
     }
     return tilesCopy;
