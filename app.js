e67b34a6d76860f534b87fe4407768a9cae4bed0
Oded Sofer <oded6560@gmail.com>
2021-10-09 16:22:50 +0300

    Arrangement

--- app.js
+++ app.js
@@ -37,9 +37,10 @@ else {
     blcs.push({ pos: secondRandom, value: 2 })
 
 }
-
 printTiles(blcs);
 document.addEventListener('keydown', (e) => { if (!e.repeat) { keyMap(e.key) } })
+
+
 function keyMap(key) {
     removeZero();
     const tilesCopy = [[...tiles[0]], [...tiles[1]], [...tiles[2]], [...tiles[3]]];
@@ -63,45 +64,36 @@ function down() {
     for (let col = 0; col < 4; col++) {
         let mergeCounter = 0;
         const newRow = [];
-        console.log('blcs' + JSON.stringify(blcs));
-
         for (let i = 3; i >= 0; i--) {
-
             if (blcs.some(element => element.pos === (i * 4 + col) && element.value !== 0)) {
                 newRow.push(...blcs.filter(element => element.pos === (i * 4 + col)));
             }
         }
         console.log('newRow' + JSON.stringify(newRow));
-
         for (let i = 0; i < newRow.length - 1; i++) {
             if (newRow[i].value === newRow[i + 1].value && newRow[i].value !== 0) {
                 newRow[i].value = newRow[i].value * 2;
                 newRow[i + 1].value = 0;
                 console.log(JSON.stringify(newRow[i + 1].pos) + '  ' + newRow[i].pos);
                 mergeCounter++;
-
             }
-
         }
-        console.log('newRow' + JSON.stringify(newRow));
-
         for (let i = 0; i < newRow.length; i++) {
             if (newRow[i].value !== 0) {
                 newRow[i].pos = (3 - i) * 4 + col;
             }
             else if (i !== 0) {
                 newRow[i].pos = newRow[i - 1].pos;
-
             }
         }
         if (mergeCounter === 2) {
-
             newRow[2].pos = 8 + col;
             newRow[3].pos = 8 + col;
-
         }
     }
 }
+
+
 function right() {
     for (let row = 0; row < 4; row++) {
         let mergeCounter = 0;
@@ -132,6 +124,8 @@ function right() {
         }
     }
 }
+
+
 function up() {
     for (let col = 0; col < 4; col++) {
         let mergeCounter = 0;
@@ -162,6 +156,7 @@ function up() {
         }
     }
 }
+
 
 function left() {
     for (let row = 0; row < 4; row++) {
@@ -194,23 +189,23 @@ function left() {
     }
 }
 
+
 function numToXY(num) {
     console.log(`p-${Math.floor(num / 4) + 1}-${(num % 4) + 1}`);
     return `p-${Math.floor(num / 4) + 1}-${(num % 4) + 1}`;
-
 }
+
+
 function printTiles(blcs) {
     for (let i = 0; i < blcs.length; i++) {
         if (blcs[i].block === undefined) {
             blcs[i].block = blocks[blcs[i].value].cloneNode(true);
             currentBlock.appendChild(blcs[i].block);
-            blcs[i].block.classList.add(numToXY(blcs[i].pos), 'new-tile', `blc${blcs[i].value}`, 'innerBlock')
-
+            blcs[i].block.classList.add(numToXY(blcs[i].pos), 'new-tile', `blc${blcs[i].value}`, 'innerBlock');
         }
-
-
     }
 }
+
 
 function addTile() {
 
@@ -219,7 +214,7 @@ function addTile() {
         blcs[i].block.innerText = blcs[i].value;
         blcs[i].block.classList.add(numToXY(blcs[i].pos), `blc${blcs[i].value}`, 'innerBlock')
         if (blcs[i].value === 0 && !blcs[i].block.classList.contains('merge')) {
-            blcs[i].block.classList.add('merge')
+            blcs[i].block.classList.add('merge');
         }
     }
 }
