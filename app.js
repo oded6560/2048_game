32c5ddbc2d3a0bdb8653b15c825b95622af4fc17
Oded Sofer <oded6560@gmail.com>
2021-10-09 18:30:14 +0300

    new tile bug fixed, now the new tule function only runs when somthing moved

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ let firstRandom = Math.floor(Math.random() * 16)
 blcs.push({ pos: firstRandom, value: 2 })
 
 //////////////////////////////////////
-// blcs.push({ pos: 2, value: 4 })
-// blcs.push({ pos: 6, value: 4 })
-// blcs.push({ pos: 10, value: 4 })
-// blcs.push({ pos: 14, value: 4 })
+// blcs.push({ pos: 12, value: 2 })
+// blcs.push({ pos: 13, value: 2 })
+// blcs.push({ pos: 14, value: 32 })
+// blcs.push({ pos: 15, value: 32 })
 //////////////////////////////////////
 
 let secondRandom = firstRandom;
@@ -35,7 +35,6 @@ if (Math.floor(Math.random() * 6) + 1 === 1) {
 }
 else {
     blcs.push({ pos: secondRandom, value: 2 })
-
 }
 printTiles(blcs);
 document.addEventListener('keydown', (e) => { if (!e.repeat) { keyMap(e.key) } })
@@ -56,28 +55,47 @@ function keyMap(key) {
     else if (key === 'ArrowRight') {
         right();
     }
-    addTile();
+    printTiles();
 }
 
 
 function down() {
+    let change = false;
     for (let col = 0; col < 4; col++) {
         let mergeCounter = 0;
-        const newRow = [];
+        const currentRow = [];
+        let newRow = [];
+        const finallRow = []
         for (let i = 3; i >= 0; i--) {
             if (blcs.some(element => element.pos === (i * 4 + col) && element.value !== 0)) {
                 newRow.push(...blcs.filter(element => element.pos === (i * 4 + col)));
             }
         }
-        console.log('newRow' + JSON.stringify(newRow));
+        for (let i = 3; i >= 4 - newRow.length; i--) {
+            console.log(JSON.stringify('newRow' + newRow + '  blcs-' + blcs[i * 4 + col] + '  i  -' + i + 'blcs  ' + blcs));
+
+            if (!blcs.some(element => element.pos === (i * 4 + col))) {
+                change = true;
+            }
+        }
         for (let i = 0; i < newRow.length - 1; i++) {
             if (newRow[i].value === newRow[i + 1].value && newRow[i].value !== 0) {
                 newRow[i].value = newRow[i].value * 2;
                 newRow[i + 1].value = 0;
-                console.log(JSON.stringify(newRow[i + 1].pos) + '  ' + newRow[i].pos);
+                i++;
                 mergeCounter++;
+                change = true;
             }
         }
+        if (mergeCounter !== 0) {
+            for (let i = 3; i >= 0; i--) {
+                if (newRow.some(element => element.pos === (i * 4 + col) && element.value !== 0)) {
+                    finallRow.push(...newRow.filter(element => element.pos === (i * 4 + col)));
+                }
+            }
+            newRow = finallRow;
+        }
+
         for (let i = 0; i < newRow.length; i++) {
             if (newRow[i].value !== 0) {
                 newRow[i].pos = (3 - i) * 4 + col;
@@ -91,24 +109,45 @@ function down() {
             newRow[3].pos = 8 + col;
         }
     }
+    if (change) {
+        console.log(JSON.stringify('change'));
+        addTile();
+    }
 }
 
 
 function right() {
+    let change = false;
     for (let row = 0; row < 4; row++) {
         let mergeCounter = 0;
-        const newRow = [];
+        let newRow = [];
+        const finallRow = []
         for (let i = 3; i >= 0; i--) {
             if (blcs.some(element => element.pos === (row * 4 + i) && element.value !== 0)) {
                 newRow.push(...blcs.filter(element => element.pos === (row * 4 + i)));
+            }
+        }
+        for (let i = 3; i >= 4 - newRow.length; i--) {
+            if (!blcs.some(element => element.pos === (row * 4 + i))) {
+                change = true;
             }
         }
         for (let i = 0; i < newRow.length - 1; i++) {
             if (newRow[i].value === newRow[i + 1].value && newRow[i].value !== 0) {
                 newRow[i].value = newRow[i].value * 2;
                 newRow[i + 1].value = 0;
+                i++;
                 mergeCounter++;
+                change = true;
             }
+        }
+        if (mergeCounter !== 0) {
+            for (let i = 3; i >= 0; i--) {
+                if (newRow.some(element => element.pos === (row * 4 + i) && element.value !== 0)) {
+                    finallRow.push(...newRow.filter(element => element.pos === (row * 4 + i)));
+                }
+            }
+            newRow = finallRow;
         }
         for (let i = 0; i < newRow.length; i++) {
             if (newRow[i].value !== 0) {
@@ -123,24 +162,45 @@ function right() {
             newRow[3].pos = 2 + 4 * row;
         }
     }
+    if (change) {
+        console.log(JSON.stringify('change'));
+        addTile();
+    }
 }
 
 
 function up() {
+    let change = false;
     for (let col = 0; col < 4; col++) {
         let mergeCounter = 0;
-        const newRow = [];
+        let newRow = [];
+        const finallRow = []
         for (let i = 0; i < 4; i++) {
             if (blcs.some(element => element.pos === (i * 4 + col) && element.value !== 0)) {
                 newRow.push(...blcs.filter(element => element.pos === (i * 4 + col)));
+            }
+        }
+        for (let i = 0; i < newRow.length; i++) {
+            if (!blcs.some(element => element.pos === (i * 4 + col))) {
+                change = true;
             }
         }
         for (let i = 0; i < newRow.length - 1; i++) {
             if (newRow[i].value === newRow[i + 1].value && newRow[i].value !== 0) {
                 newRow[i].value = newRow[i].value * 2;
                 newRow[i + 1].value = 0;
+                i++;
                 mergeCounter++;
+                change = true;
             }
+        }
+        if (mergeCounter !== 0) {
+            for (let i = 0; i < 4; i++) {
+                if (newRow.some(element => element.pos === (i * 4 + col) && element.value !== 0)) {
+                    finallRow.push(...newRow.filter(element => element.pos === (i * 4 + col)));
+                }
+            }
+            newRow = finallRow;
         }
         for (let i = 0; i < newRow.length; i++) {
             if (newRow[i].value !== 0) {
@@ -155,24 +215,45 @@ function up() {
             newRow[3].pos = 4 + col;
         }
     }
+    if (change) {
+        console.log(JSON.stringify('change'));
+        addTile();
+    }
 }
 
 
 function left() {
+    let change = false;
     for (let row = 0; row < 4; row++) {
         let mergeCounter = 0;
-        const newRow = [];
+        let newRow = [];
+        const finallRow = []
         for (let i = 0; i < 4; i++) {
             if (blcs.some(element => element.pos === (row * 4 + i) && element.value !== 0)) {
                 newRow.push(...blcs.filter(element => element.pos === (row * 4 + i)));
+            }
+        }
+        for (let i = 0; i < newRow.length; i++) {
+            if (!blcs.some(element => element.pos === (row * 4 + i))) {
+                change = true;
             }
         }
         for (let i = 0; i < newRow.length - 1; i++) {
             if (newRow[i].value === newRow[i + 1].value && newRow[i].value !== 0) {
                 newRow[i].value = newRow[i].value * 2;
                 newRow[i + 1].value = 0;
+                i++;
                 mergeCounter++;
+                change = true;
             }
+        }
+        if (mergeCounter !== 0) {
+            for (let i = 0; i < 4; i++) {
+                if (newRow.some(element => element.pos === (row * 4 + i) && element.value !== 0)) {
+                    finallRow.push(...newRow.filter(element => element.pos === (row * 4 + i)));
+                }
+            }
+            newRow = finallRow;
         }
         for (let i = 0; i < newRow.length; i++) {
             if (newRow[i].value !== 0) {
@@ -187,6 +268,10 @@ function left() {
             newRow[3].pos = 1 + 4 * row;
         }
     }
+    if (change) {
+        console.log(JSON.stringify('change'));
+        addTile();
+    }
 }
 
 
@@ -196,12 +281,19 @@ function numToXY(num) {
 }
 
 
-function printTiles(blcs) {
+function printTiles() {
     for (let i = 0; i < blcs.length; i++) {
         if (blcs[i].block === undefined) {
             blcs[i].block = blocks[blcs[i].value].cloneNode(true);
             currentBlock.appendChild(blcs[i].block);
-            blcs[i].block.classList.add(numToXY(blcs[i].pos), 'new-tile', `blc${blcs[i].value}`, 'innerBlock');
+            blcs[i].block.classList.add('new-tile');
+        } else {
+            blcs[i].block.className = '';
+        }
+        blcs[i].block.innerText = blcs[i].value;
+        blcs[i].block.classList.add(numToXY(blcs[i].pos), `blc${blcs[i].value}`, 'innerBlock')
+        if (blcs[i].value === 0) {
+            blcs[i].block.classList.add('merge');
         }
     }
 }
@@ -209,14 +301,24 @@ function printTiles(blcs) {
 
 function addTile() {
 
-    for (let i = 0; i < blcs.length; i++) {
-        blcs[i].block.className = '';
-        blcs[i].block.innerText = blcs[i].value;
-        blcs[i].block.classList.add(numToXY(blcs[i].pos), `blc${blcs[i].value}`, 'innerBlock')
-        if (blcs[i].value === 0 && !blcs[i].block.classList.contains('merge')) {
-            blcs[i].block.classList.add('merge');
+
+    const zeroCounter = [];
+    for (let i = 0; i < 16; i++) {
+        if (!blcs.some(element => element.pos === i)) {
+            zeroCounter.push(i);
         }
     }
+    let randomTitle = zeroCounter[Math.floor(Math.random() * zeroCounter.length)];
+    let newTileValue;
+    if (Math.floor(Math.random() * 6) + 1 === 1) {
+        newTileValue = 4;
+    }
+    else {
+        newTileValue = 2;
+    }
+    blcs.push({ pos: randomTitle, value: newTileValue })
+    printTiles();
+
 }
 function removeZero() {
     for (let i = 0; i < blcs.length; i++) {
